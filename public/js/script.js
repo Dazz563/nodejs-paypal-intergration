60ae31e90f52aac17db9d95cf974acd765e508b6
Darren <58746026+Dazz563@users.noreply.github.com>
2023-04-12 12:59:14 +0200

    comments added

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,13 @@
 paypal
 	.Buttons({
+		// When the button is clicked, create a new order using the /create-order route and return the order ID
 		createOrder: function () {
 			return fetch('/create-order', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
+				// Send the order details as a JSON payload
 				body: JSON.stringify({
 					items: [
 						{
@@ -20,18 +22,23 @@ paypal
 				}),
 			})
 				.then((res) => {
+					// If the response is OK, parse the response body as JSON and return the order ID
 					if (res.ok) return res.json();
+					// Otherwise, reject the promise with the JSON error message
 					return res.json().then((json) => Promise.reject(json));
 				})
 				.then(({id}) => {
 					return id;
 				})
 				.catch((e) => {
+					// Log any errors to the console
 					console.error(e.error);
 				});
 		},
+		// When the payment is approved, capture the payment using the PayPal API
 		onApprove: function (data, actions) {
 			return actions.order.capture();
 		},
 	})
+	// Render the PayPal button in the element with ID "paypal"
 	.render('#paypal');
--- src/controllers/paypal.controller.js
+++ src/controllers/paypal.controller.js
@@ -1,22 +1,35 @@
+// Importing the required module(s)
 const paypal = require('@paypal/checkout-server-sdk');
 
+// Setting up the environment for API calls based on the environment (Production/Sandbox)
 const Environment = process.env.NODE_ENV === 'production' ? paypal.core.LiveEnvironment : paypal.core.SandboxEnvironment;
+
+// Creating a new PayPal client using the environment and client ID and secret from the environment variables
 const paypalClient = new paypal.core.PayPalHttpClient(new Environment(process.env.PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET));
 
+// Creating a map of store items
 const storeItems = new Map([
 	[1, {price: 100, name: 'Learn React Today'}],
 	[2, {price: 200, name: 'Learn CSS Today'}],
 ]);
 
+// Exporting a function to render the index page
 exports.getIndex = async (req, res, next) => {
+	// Rendering the index page with the PayPal client ID passed as a variable
 	res.render('index', {paypalClientId: process.env.PAYPAL_CLIENT_ID});
 };
 
+// Exporting a function to create a new PayPal order
 exports.createOrder = async (req, res, next) => {
+	// Creating a new OrdersCreateRequest object
 	const request = new paypal.orders.OrdersCreateRequest();
+
+	// Calculating the total price of the order based on the items in the request body
 	const total = req.body.items.reduce((sum, item) => {
 		return sum + storeItems.get(item.id).price * item.quantity;
 	}, 0);
+
+	// Setting the request parameters
 	request.prefer('return=representation');
 	request.requestBody({
 		intent: 'CAPTURE',
@@ -48,10 +61,13 @@ exports.createOrder = async (req, res, next) => {
 	});
 
 	try {
+		// Executing the request and getting the order details
 		const order = await paypalClient.execute(request);
 		console.log('Here is your order: ', order);
+		// Sending the order ID as a response
 		res.json({id: order.result.id});
 	} catch (e) {
+		// Handling errors and sending an error response
 		res.status(500).json({error: e.message});
 	}
 };
